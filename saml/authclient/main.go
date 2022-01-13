873b0fac0d852b7af4fad7d9ab32147dc481fa43
Zheng Nie <znie@fortinet.com>
2022-01-12 23:32:43 -0800

    Add FAC API Auth support

--- saml/authclient/main.go
+++ saml/authclient/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"automation/authclient/args"
+	"automation/authclient/facapi"
 	"automation/authclient/interfaces"
 	"automation/authclient/saml"
 	"crypto/tls"
 	"log"
 	"net/http"
 	"strings"
+	"time"
 )
 
 func switchOffHttpsVerify() {
@@ -15,19 +17,52 @@ func switchOffHttpsVerify() {
 		&tls.Config{InsecureSkipVerify: true}
 }
 
+func Run(idx int, c chan interface{}) {
+	runner := getRunner()
+	runner.Setup(idx)
+
+	var pass int64 = 0
+	var fail int64 = 0
+	var complete int64
+	t1 := time.Now()
+
+	for complete = 0; complete < args.REPEAT; complete++ {
+		if runner.Run() == true {
+			pass++
+		} else {
+			fail++
+		}
+	}
+
+	t2 := time.Now()
+	total := t2.Sub(t1)
+	log.Printf(
+		"%s IDX: %d test took %f seconds, %f ms per request",
+		args.AUTHMODE, idx, total.Seconds(), float64(total.Milliseconds()/args.REPEAT),
+	)
+	c <- []int64{
+		int64(idx),
+		int64(total.Seconds()),
+		int64(total.Milliseconds() / args.REPEAT),
+		complete, pass, fail,
+	}
+}
+
 func getRunner() interfaces.Runner {
 	switch strings.ToLower(args.AUTHMODE) {
 	case "saml":
 		return &saml.SamlRunner{}
+	case "facapi":
+		return &facapi.FacApiAuthRunner{}
 	default:
 		return &saml.SamlRunner{}
 	}
 }
 
-func dispatchJob(runner interfaces.Runner) {
+func dispatchJob() {
 	c := make(chan interface{})
 	for i := 0; i < args.CONCURRENT; i++ {
-		go runner.Run(i, c)
+		go Run(i, c)
 	}
 	for i := 0; i < args.CONCURRENT; i++ {
 		log.Printf("%v\n", <-c)
@@ -38,6 +73,5 @@ func main() {
 	if args.DISABLE_SSL_VERIFY == true {
 		switchOffHttpsVerify()
 	}
-	runner := getRunner()
-	dispatchJob(runner)
+	dispatchJob()
 }
