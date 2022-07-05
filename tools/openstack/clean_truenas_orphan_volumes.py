cf6939a54c3bb6b0302462d7ac81f53fc845c6c0
Zheng Nie <znie@fortient.com>
2022-07-05 00:41:28 -0700

    add all project support for clean up orphan volumes

--- tools/openstack/clean_truenas_orphan_volumes.py
+++ tools/openstack/clean_truenas_orphan_volumes.py
@@ -5,17 +5,29 @@ import requests
 
 conn = openstack.connect("stack1")
 host = "stack-1-store1@truenas-1#truenas-1"
-api_key = "xxxxxxxxx"
+api_key = "2-4hrKAUs2VQq7rducIraXD8vZT93DX8Uh1UzqcALTIvhDOriGZClvvliomIeo84Ca"
 base_url = "http://10.160.83.8/api/v2.0"
 
 volumes_not_delete = []
 volumes_to_delete = []
+exclude_project = ["service"]
+pool_name = "storage"
+dataset_name = "storage/cinder"
 
 
 def get_current_volume_ids():
-    for volume in conn.list_volumes():
-        if host in volume.host:
-            volumes_not_delete.append(volume.id.split("-")[0])
+    global conn
+    projects = conn.list_projects()
+    for proj in projects:
+        if proj.name in exclude_project:
+            continue
+        print(f"checking project: {proj.name}")
+        conn = conn.connect_as_project(proj)
+        for volume in conn.list_volumes():
+            if host in volume.host:
+                vol = volume.id.split("-")[0]
+                print(f"Found using volumes: {vol}")
+                volumes_not_delete.append(vol)
 
 
 def request(method, api):
@@ -24,15 +36,18 @@ def request(method, api):
         method, url, headers={
             'Content-Type': 'application/json',
             "Authorization": f"Bearer {api_key}"
-        }
+        }, verify=False
     )
 
 
 def get_all_volumes():
     res = request("GET", "/iscsi/target")
     targets = {}
-    for target in res.json():
-        targets[target["name"]] = target["id"]
+    if res.status_code == 200:
+        for target in res.json():
+            targets[target["name"]] = target["id"]
+    else:
+        raise Exception(f"Error when get volumes from truenas, {res.text}")
 
     textends = {}
     res = request("GET", "/iscsi/targetextent")
@@ -41,25 +56,35 @@ def get_all_volumes():
 
     res = request("GET", "/pool/dataset")
     if res.status_code == 200:
-        volumes = res.json()[1]["children"]
-        for volume in volumes:
-            vol_id = volume["name"]
-            v_id = vol_id.split("-")[1]
-            if v_id in volumes_not_delete:
-                continue
-            volumes_to_delete.append(v_id)
-            res = request(
-                "DELETE", f"/pool/dataset/id/{urllib.parse.quote_plus(vol_id)}"
-            )
-            if res.status_code == 200:
-                print(f"deleted zvol volume-{v_id}")
-            t_id = targets.get(f"target-{v_id}")
-            res = request("DELETE", f"/iscsi/target/id/{t_id}")
-            if res.status_code == 200:
-                print(f"deleted iscsi target target-{t_id}")
+        for pool in res.json():
+            if pool["id"] == pool_name:
+                datasets = pool["children"]
+                ds = list(
+                    filter(lambda x: x["id"] == dataset_name, datasets)
+                )[-1]
+                if ds:
+                    for volume in ds["children"]:
+                        vol_id = volume["name"]
+                        v_id = vol_id.split("-")[1]
+                        if v_id in volumes_not_delete:
+                            continue
+                        volumes_to_delete.append(v_id)
+                        print(f"volume to delete:  {v_id}")
+                        res = request(
+                            "DELETE",
+                            f"/pool/dataset/id/"
+                            f"{urllib.parse.quote_plus(vol_id)}"
+                        )
+                        if res.status_code == 200:
+                            print(f"deleted zvol volume-{v_id}")
+                        t_id = targets.get(f"target-{v_id}")
+                        res = request("DELETE", f"/iscsi/target/id/{t_id}")
+                        if res.status_code == 200:
+                            print(f"deleted iscsi target target-{t_id}")
+                else:
+                    print(f"No data set {dataset_name} found")
 
 
 get_current_volume_ids()
-print(volumes_not_delete)
 get_all_volumes()
 
